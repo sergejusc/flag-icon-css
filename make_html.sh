cfea5fb62147dd54149d988f127685451fa33998
Sergey <no@mail.com>
2016-08-25 23:43:44 +0300

    Fix for DOM module id

--- make_html.sh
+++ make_html.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 touch flag-icon-css.html
-echo '<dom-module id="app-theme"><template><style is="flag-icon-css">' > flag-icon-css.html
+echo '<dom-module id="flag-icon-css"><template><style is="flag-icon-css">' > flag-icon-css.html
 cat css/flag-icon.min.css >> flag-icon-css.html
 echo '' >> flag-icon-css.html
 echo '</style></template></dom-module>' >> flag-icon-css.html

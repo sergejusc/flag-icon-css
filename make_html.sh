95beb55e51c13e39ca5bde08171c1c7222ee1d29
Sergey <no@mail.com>
2016-08-25 23:45:54 +0300

    Fix for html path

--- make_html.sh
+++ make_html.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-touch flag-icon-css.html
-echo '<dom-module id="flag-icon-css"><template><style is="flag-icon-css">' > flag-icon-css.html
-cat css/flag-icon.min.css >> flag-icon-css.html
-echo '' >> flag-icon-css.html
-echo '</style></template></dom-module>' >> flag-icon-css.html
+touch html/flag-icon-css.html
+echo '<dom-module id="flag-icon-css"><template><style is="flag-icon-css">' > html/flag-icon-css.html
+cat css/flag-icon.min.css >> html/flag-icon-css.html
+echo '' >> html/flag-icon-css.html
+echo '</style></template></dom-module>' >> html/flag-icon-css.html
